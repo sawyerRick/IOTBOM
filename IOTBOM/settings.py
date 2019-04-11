d4953ca8d78db0f486fa716e2d368c1d0c53d72a
sawyer <sawyer@sawyers-MacBook-Pro.local>
2019-04-11 15:33:30 +0800

    add mysql

--- IOTBOM/settings.py
+++ IOTBOM/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'IOTBOM.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'IOTBOM',
+        'USER': 'root',
+        'PASSWORD': 'ksk123456',
+        'HOST': 'coksk.cn',
+        'PORT': '3306',
     }
 }
 
